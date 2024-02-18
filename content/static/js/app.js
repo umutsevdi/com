ec42af0f897cf8328f9f11869b34aec5ec874e8d
Umut Sevdi <sevdiumut@pm.me>
2024-02-18 22:30:35 +0300

    Added animation for bottom drawer

--- content/static/js/app.js
+++ content/static/js/app.js
@@ -1,20 +1,12 @@
 window.addEventListener('load', function() {
+    const navToggle = document.querySelector('.nav-toggle');
+    const nav = document.querySelector('.nav');
+    navToggle.addEventListener('click', () => nav.classList.toggle('active'));
     var currentUrl = window.location.pathname;
-
     if (currentUrl === '/') {
         home();
-    }
+    };
 });
-
-function setNavbar() {
-    document.addEventListener('DOMContentLoaded', () => {
-        const navToggle = document.querySelector('.nav-toggle');
-        const nav = document.querySelector('.nav');
-        navToggle.addEventListener('click',
-            () => nav.classList.toggle('active'));
-    });
-}
-setNavbar();
 
 function copyToClipboard(item) {
     var txt = document.getElementById(item);
@@ -22,19 +14,20 @@ function copyToClipboard(item) {
     txt.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(txt.value);
     alert("Copied the text: " + txt.value);
-}
+};
 
 function home() {
     var projectDrawer = document.getElementById('project-drawer');
     var summary = document.getElementById('project-drawer-summary');
-    var img = document.getElementById('project-drawer-img');
-
-    projectDrawer.addEventListener('toggle',
-        () => {
-            window.scrollTo(0, document.body.scrollHeight);
-            summary.innerText = "Show " + (projectDrawer.open ? "Less" : "More");
-            img.classList.toggle("project-drawer-animation")
-        });
-}
+    var arrows = [
+        document.getElementById('project-drawer-img-up'),
+        document.getElementById('project-drawer-img-down')
+    ];
+    projectDrawer.addEventListener('toggle', () => {
+        window.scrollTo(0, document.body.scrollHeight);
+        summary.innerText = "Show " + (projectDrawer.open ? "Less" : "More");
+        arrows.forEach((arrow) => arrow.classList.toggle("hidden"));
+    });
+};
 
 
